0861dc637cf734f07b6fc71dbeeaac73db7f35cc
niraj994u <niraj994u@gmail.com>
2025-10-04 16:06:42 +0530

    added swagger dependency

--- src/main/java/com/striker/auth/entity/Auditing.java
+++ src/main/java/com/striker/auth/entity/Auditing.java
@@ -1,24 +1,9 @@
 package com.striker.auth.entity;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
+import lombok.Data;
 
-import java.time.LocalDateTime;
-
-@Setter
-@Getter
-public abstract class Auditing {
-    @CreatedBy
+@Data
+public class Auditing {
     private String createdBy;
-    @CreatedDate
-    private LocalDateTime createdAt;
-    @LastModifiedBy
     private String updatedBy;
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
-
 }
